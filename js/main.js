15e9224dc33ddfc7748c47e228f4859de8dea004
Eyþór Konráðsson <kh.krimper@gmail.com>
2022-02-20 14:38:10 +0100

    Changing mobile functionality on footnotes

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-console.log('heloooo')
+window.onload = function() {
+    const footnotes = document.querySelectorAll(".title__wrapp");
+    footnotes.forEach((footnote) => {
+        footnote.addEventListener("click", (e) => {
+            const parent = e.target.parentNode;
+            const footnote = parent.querySelector('.title__card')
+            if (footnote) {
+                if (!footnote.classList.contains("title__card--open")) {
+                    document.querySelectorAll('.title__card--open').forEach((item) => {
+                        item.classList.remove('title__card--open')
+                    })
+                    footnote.classList.add('title__card--open')
+                }
+            }
+        }, false);
+    })
+    const closeFootnotes = document.querySelectorAll('.title__close')
+    closeFootnotes.forEach((closeElem) => {
+        closeElem.addEventListener("click", (e) => {
+            const parent = e.target.parentNode.parentNode;
+            if (parent) {
+                if (parent.classList.contains("title__card--open")) {
+                    parent.classList.remove('title__card--open')
+                }
+            }
+        }, false);
+    })
+};
+  
